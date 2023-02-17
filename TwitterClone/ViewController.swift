c455c4da9294e79a37363f2bd07cbdc619321bd1
JmOliveUnion <jm@oliveunion.com>
2023-02-17 16:27:37 +0900

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by 김민종 on 2023/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
